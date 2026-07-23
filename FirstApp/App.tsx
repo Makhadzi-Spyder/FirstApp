e7b9a80a57ce294a42c6215aa190cf1934a75a42
Makhadzi-Spyder <mmbobvu@gmail.com>
2026-07-23 16:28:21 +0200

    Styling UI
    Made bold text
    Added line
    Added Image

--- FirstApp/App.tsx
+++ FirstApp/App.tsx
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.welcomeText}>
+      <View style={styles.mainPicture}>
+        <Image style={styles.ImageSize} source={require('./images/Logo.jpeg')} />
+      </View>
+      <Text style={styles.welcomeText}>Welcome to your app!</Text>
+      <Text style={styles.label}>Enter Name:</Text>
+      <TextInput placeholder="Jane" />
+      <View style={styles.line} />
+      <Text style={styles.label}>Enter Surname:</Text>
+      <TextInput placeholder="Doe" />
+      <Button title = "Add user" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    welcomeText: {
+      paddingTop: 30,
+      color: 'purple',
+      fontWeight: 'bold',
+      fontSize: 28,
+      textAlign: 'center',
+    },
+  
+    line: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    width: '100%',
+    marginVertical: 10,
+  },
+
+  label:{
+    fontWeight: 'bold',
+  },
+
+  mainPicture: {
+    paddingTop: 30,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  ImageSize: {
+    width: 200,
+    height: 200,
   },
 });
