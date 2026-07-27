0ba21192939c76d5226843fbe83450a68c31b195
Makhadzi-Spyder <mmbobvu@gmail.com>
2026-07-27 12:32:53 +0200

    Flew update
    Fixed errors
    Added flex
    Added MainScreen

--- FirstApp/App.tsx
+++ FirstApp/App.tsx
@@ -1,27 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View, Image } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, Image} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 export default function App() {
+  
+  const Stack = createNativeStackNavigator();
+
+  return (
+
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={MainScreen} />
+    </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function MainScreen() {
+
+  const [Name, setName] = useState('');
+  const [Surname, setSurname] = useState('');
+
+  console.log("App works!");
+
   return (
     <View style={styles.welcomeText}>
+
       <View style={styles.mainPicture}>
         <Image style={styles.ImageSize} source={require('./images/Logo.jpeg')} />
       </View>
+
       <Text style={styles.welcomeText}>Welcome to your app!</Text>
-      <Text style={styles.label}>Enter Name:</Text>
-      <TextInput placeholder="Jane" />
-      <View style={styles.line} />
-      <Text style={styles.label}>Enter Surname:</Text>
-      <TextInput placeholder="Doe" />
-      <Button title = "Add user" />
+
+
+        <View style={styles.InputFlex}> 
+         <Text style={styles.label}>Enter Name:</Text>
+         <TextInput placeholder="Jane" 
+         onChangeText={newText => setName(newText)}/>
+
+         
+         <Text style={styles.label}>Enter Surname:</Text>
+         <TextInput placeholder="Doe" 
+         onChangeText={(newText) => setSurname(newText)}/>
+        </View>
+
+
+      <Button title = "Add user" 
+       onPress={() => {
+        console.log("Name: " + Name + ""+ " Surname: " + Surname);
+       }}/>
+
+      <StatusBar style="auto" />
+
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
     welcomeText: {
       paddingTop: 30,
-      color: 'purple',
+      color: 'lightblue',
       fontWeight: 'bold',
       fontSize: 28,
       textAlign: 'center',
@@ -29,7 +70,7 @@ const styles = StyleSheet.create({
   
     line: {
     borderBottomColor: 'black',
-    borderBottomWidth: 1,
+    borderBottomWidth: 1, //only part needed to make the line visible
     width: '100%',
     marginVertical: 10,
   },
@@ -47,5 +88,10 @@ const styles = StyleSheet.create({
   ImageSize: {
     width: 200,
     height: 200,
+  },
+
+  InputFlex: {
+    flexDirection: 'row',
+    marginTop: 10,
   },
 });
